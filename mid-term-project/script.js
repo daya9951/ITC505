ef12c0734d74f72835cd11b32ac98f7958b16c1c
daya9951 <dayakarreddy582@gmail.com>
2025-03-31 22:46:27 -0700

    Update script.js

--- mid-term-project/script.js
+++ mid-term-project/script.js
@@ -1,4 +1,3 @@
-// script.js
 const storyData = {
   start: {
     text: "You are a Jedi Padawan at the Jedi Temple. What will you do?",
@@ -6,7 +5,7 @@ const storyData = {
       { text: "Go on a secret mission to Tatooine", next: "tatooine" },
       { text: "Stay and train with Master Yoda", next: "yoda" }
     ],
-    image: "<img src="1.jpg" alt=\"Jedi Temple\">"
+    image: "<img src='1.jpg' alt='Jedi Temple'>"
   },
   tatooine: {
     text: "You arrive on Tatooine. You find a mysterious Sith artifact in a cave.",
@@ -14,7 +13,7 @@ const storyData = {
       { text: "Use the artifact", next: "artifact_master" },
       { text: "Destroy it", next: "darkness" }
     ],
-    image: "<img src="2.jpg" alt=\"Tatooine\">"
+    image: "<img src='2.jpg' alt='Tatooine'>"
   },
   yoda: {
     text: "Master Yoda begins your intense training. It is rigorous and enlightening.",
@@ -22,27 +21,27 @@ const storyData = {
       { text: "Continue and master the training", next: "jedi_master" },
       { text: "Leave to pursue forbidden knowledge", next: "banished" }
     ],
-    image: "<img src="3.jpg" alt=\"Yoda Training\">"
+    image: "<img src='3.jpg' alt='Yoda Training'>"
   },
   artifact_master: {
     text: "You master the Sith artifact and gain power without falling. You are a balance of light and dark.",
     choices: [],
-    image: "<img src="4.jpg" alt=\"Artifact Master\">"
+    image: "<img src='4.jpg' alt='Artifact Master'>"
   },
   darkness: {
     text: "The artifact corrupts you. You become a Sith Lord feared across the galaxy.",
     choices: [],
-    image: "<img src="5.jpg" alt=\"Sith Lord\">"
+    image: "<img src='5.jpg' alt='Sith Lord'>"
   },
   jedi_master: {
     text: "Your dedication pays off. You become a Jedi Master and serve the Republic wisely.",
     choices: [],
-    image: "<img src="6.jpg" alt=\"Jedi Master\">"
+    image: "<img src='6.jpg' alt='Jedi Master'>"
   },
   banished: {
     text: "You gain knowledge but are exiled. You live in solitude, studying ancient powers.",
     choices: [],
-    image: "<img src="7.jpg" alt=\"Banished Jedi\">"
+    image: "<img src='7.jpg' alt='Banished Jedi'>"
   },
   duel: {
     text: "You face Darth Vader. Your strength is tested in an epic duel.",
@@ -50,17 +49,17 @@ const storyData = {
       { text: "Defeat Vader", next: "victory" },
       { text: "Join Vader", next: "apprentice" }
     ],
-    image: "<img src="8.jpg" alt=\"Lightsaber Duel\">"
+    image: "<img src='8.jpg' alt='Lightsaber Duel'>"
   },
   victory: {
     text: "You defeat Vader and restore balance to the Force. The galaxy celebrates your heroism.",
     choices: [],
-    image: "<img src="9.jpg" alt=\"Victory\">"
+    image: "<img src='9.jpg' alt='Victory'>"
   },
   apprentice: {
     text: "Vader defeats you and turns you into his apprentice. The galaxy trembles.",
     choices: [],
-    image: "<img src="10.jpg" alt=\"Sith Apprentice\">"
+    image: "<img src='10.jpg' alt='Sith Apprentice'>"
   }
 };
 
